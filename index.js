bc0513ca231d30a8f2a67ba993f195fa6409e87a
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-06-25 13:21:44 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function introduction(name) {
+    return(`Hi, my name is ${name}.`);  
+}
+console.log(introduction('Aki'))
+console.log(introduction('Samip'))
+
+function introductionWithLanguage(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);    
+}
+
+console.log(introductionWithLanguage('Aki', 'Ember.js')); 
+console.log(introductionWithLanguage("Samip", "React"));
+
+
+function introductionWithLanguageOptional(name, language = 'JavaScript') {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+console.log( introductionWithLanguageOptional("Gracie"));
+
+
+function introductionWithLanguageOptional(name, language ='JavaScript') {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+const result = introductionWithLanguageOptional('Gracie', 'Python');
+console.log(result);
